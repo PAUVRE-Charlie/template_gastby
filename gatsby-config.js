d4e8d173a80e7bcbe20997060c459f5f88d9779e
Charlie Pauvré <charlie.mangakan@gmail.com>
2022-02-27 13:29:52 +0100

    fix export type in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,31 @@
 // The default metadata that will be used in the SEO component
-export const siteMetadata = {
-  title: '', // the default name of the tab
-  description: '', // the description of your website
-  image: '', // image that appears below the link when sharing
-  url: '', // the url where your site will be
-};
+emodule.export = {
+  siteMetadata: {
+    title: '', // the default name of the tab
+    description: '', // the description of your website
+    image: '', // image that appears below the link when sharing
+    url: '', // the url where your site will be
+  },
 
-/*
+  /*
     The librairies added to the default gastby starter pack. To in
 
     npm install --save-dev prettier gatsby-plugin-preact preact preact-render-to-string gatsby-plugin-image gatsby-plugin-sharp gatsby-source-filesystem gatsby-transformer-sharp gatsby-plugin-react-helmet
 */
-export const plugins = [
-  'gatsby-transformer-sharp',
-  'gatsby-plugin-sharp',
-  'gatsby-plugin-image',
-  'gatsby-plugin-react-helmet', // to use a SEO component (that will contain the metadata of your page)
-  'gatsby-plugin-preact', // to use preact instead of react
-  {
-    resolve: 'gatsby-source-filesystem', // the location of your images for queries
-    options: {
-      name: 'images',
-      path: `${__dirname}/src/images/`,
+  plugins: [
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet', // to use a SEO component (that will contain the metadata of your page)
+    'gatsby-plugin-preact', // to use preact instead of react
+    {
+      resolve: 'gatsby-source-filesystem', // the location of your images for queries
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
     },
-  },
-  /*
+    /*
   The config section necessary to use react-i18next (to obtain a multi-language website)
   --- to install it, use npm install --save gatsby-plugin-react-i18next i18next react-i18next
 
@@ -53,4 +54,5 @@ export const plugins = [
     },
   },
   */
-];
+  ],
+};

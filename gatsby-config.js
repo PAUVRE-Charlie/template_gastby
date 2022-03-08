60ecc7e8b7eab8cb9b173ab9d0197d71763ab879
Charlie Pauvré <chpauvre@gmail.com>
2022-03-08 20:39:18 +0100

    set up the pwa and layout component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,42 @@
 // The default metadata that will be used in the SEO component
 module.exports = {
   siteMetadata: {
-    title: '', // the default name of the tab
-    description: '', // the description of your website
+    title: 'Gatsby project', // the default name of the tab
+    description: 'Gatsby project', // the description of your website
     image: '', // image that appears below the link when sharing
     url: '', // the url where your site will be
   },
-  
+
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`, // must be the first plugin
+      options: {
+        name: 'Gatsby project',
+        short_name: 'Gatsby project',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        display: 'minimal-ui',
+        icon: 'static/logo/favicon-32x32.png', // use https://realfavicongenerator.net/ to generate icons
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet', // to use a SEO component (that will contain the metadata of your page)
     'gatsby-plugin-preact', // to use preact instead of react
+    'gatsby-plugin-preload-fonts', // to preload all web fonts
+    'gatsby-plugin-web-font-loader', // to load asyncronously all web fonts
+    'gatsby-plugin-purgecss', // to remove all unused css,
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: [
+          // 'https://foo.com',
+        ],
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem', // the location of your images for queries
       options: {
---
+++ src/layouts/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { useLocation } from '@reach/router';
+import { useStaticQuery, graphql } from 'gatsby';
+// import { useI18next } from 'gatsby-plugin-react-i18next';
+// import { useTranslation } from 'react-i18next';
+import './styles.css';
+
+const Layout = ({ title, description, image, children }) => {
+  const { pathname } = useLocation();
+  // const { language } = useI18next();
+  const { site } = useStaticQuery(query);
+  const { defaultTitle, defaultDescription, defaultImage, siteUrl } = site.siteMetadata;
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname}`,
+  };
+
+  return (
+    <div id="app">
+      <Helmet
+        title={seo.title}
+        htmlAttributes={{
+          // lang: language,
+          lang: 'en',
+        }}
+      >
+        <meta name='description' content={seo.description} />
+        <meta name='image' content={seo.image} />
+        {seo.url && <meta property='og:url' content={seo.url} />}
+        {seo.title && <meta property='og:title' content={seo.title} />}
+        {seo.description && <meta property='og:description' content={seo.description} />}
+        {seo.image && <meta property='og:image' content={seo.image} />}
+        <meta name='twitter:card' content='summary_large_image' />
+        {seo.title && <meta name='twitter:title' content={seo.title} />}
+        {seo.description && <meta name='twitter:description' content={seo.description} />}
+        {seo.image && <meta name='twitter:image' content={seo.image} />}
+        <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Helmet>
+      <header>
+          {/* create the header here */}
+      </header>
+      <div id='content'>
+          {children}
+      </div>
+      <footer>
+          {/* create the footer here */}
+      </footer>
+    </div>
+  );
+};
+export default Layout;
+
+Layout.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+};
+Layout.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+};
+
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        defaultDescription: description
+        siteUrl: url
+        defaultImage: image
+      }
+    }
+  }
+`;
